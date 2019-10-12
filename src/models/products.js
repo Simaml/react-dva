f586988add749f27803b7ac7f266b9d8f8f48a75
mingliang.sima <mingliang.sima@mobilemd.cn>
2019-10-12 15:56:32 +0800

    style:第四次提交

--- src/models/products.js
+++ src/models/products.js
@@ -1,10 +1,8 @@
 export default {
     namespace: 'products',
-
     state: [],
-
+    //副作用，主要是异步处理
     effects: {},
-
     reducers: {
         delete(state, { payload: id }) {
             return state.filter(item => item.id !== id);
